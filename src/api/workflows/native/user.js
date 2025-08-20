49021477a972be13e7e4b86b3d605eb6328bc1ef
John Wregglesworth <wregglej@cyverse.org>
2025-08-20 12:26:09 -0700

    Remove IRODS and ldap calls from the user workflows

--- src/api/workflows/native/user.js
+++ src/api/workflows/native/user.js
@@ -20,37 +20,6 @@ async function userCreationWorkflow(user) {
 
     logger.info(`Running native workflow for user ${user.username}: creation`)
 
-    // LDAP: create user
-    await ldapCreateUser(user)
-
-    // LDAP: set user password
-    await ldapChangePassword(user.username, user.password)
-
-    // LDAP: add user to groups
-    await ldapAddUserToGroup(user.username, process.env['LDAP_EVERYONE_GROUP'])
-    await ldapAddUserToGroup(user.username, 'community')
-
-    // IRODS: create user
-    await irodsCreateUser(user.username)
-
-    // IRODS: set user password
-    await irodsChangePassword(user.username, user.password)
-
-    // IRODS: grant access to user directory
-    if (process.env.IRODS_IPCSERVICES_ENABLED === 'true')
-        await irodsChMod(
-            'own',
-            'ipcservices',
-            `/${process.env['IRODS_ZONE_NAME']}/home/${user.username}`
-        )
-
-    // IRODS: grant access to user directory
-    await irodsChMod(
-        'own',
-        'rodsadmin',
-        `/${process.env['IRODS_ZONE_NAME']}/home/${user.username}`
-    )
-
     // Mailchimp: subscribe user to newsletter
     if (process.env.MAILCHIMP_ENABLED.toLowerCase() === 'true')
         await mailchimpSubscribe(user.email, user.first_name, user.last_name)
@@ -62,16 +31,6 @@ async function userPasswordUpdateWorkflow(user) {
     logger.info(
         `Running native workflow for user ${user.username}: password update`
     )
-
-    // LDAP: update user password
-    await ldapChangePassword(user.username, user.password)
-
-    // LDAP: update shadowLastChange
-    const daysSinceEpoch = Math.floor(new Date() / 8.64e7).toString()
-    await ldapModify(user.username, 'shadowLastChange', daysSinceEpoch)
-
-    // IRODS: set user password
-    await irodsChangePassword(user.username, user.password)
 }
 
 // Based on v1 portal:/account/views/user.py:perform_destroy()
@@ -79,21 +38,6 @@ async function userDeletionWorkflow(user) {
     if (!user || !user.emails) throw 'Missing required property'
 
     logger.info(`Running native workflow for user ${user.username}: deletion`)
-
-    // LDAP: delete user
-    try {
-        await ldapDeleteUser(user.username)
-    } catch (e) {
-        console.error(e)
-    }
-
-    // IRODS: delete user
-    try {
-        await irodsSafeDeleteHome(user.username)
-        await irodsDeleteUser(user.username)
-    } catch (e) {
-        console.error(e)
-    }
 
     // Mailchimp: unsubscribe user from newsletter
     if (process.env.MAILCHIMP_ENABLED.toLowerCase() == 'true') {
